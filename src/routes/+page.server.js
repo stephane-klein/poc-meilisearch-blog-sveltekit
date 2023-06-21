b54bee7e5356818c4877ea5477888bff4b6aba52
Stéphane Klein <contact@stephane-klein.info>
2023-06-21 17:36:26 +0200

    Setup search form

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,14 @@
 import { MeiliSearch } from 'meilisearch';
 
-export async function load() {
+export async function load(event) {
     const client = new MeiliSearch({ host: 'http://localhost:7700' });
 
-    return await client.index('posts').search('');
+    return await client.index('posts').search(event.locals?.search);
 }
+
+export const actions = {
+    default: async(event) => {
+        const data = await event.request.formData();
+        event.locals.search = data.get('search');
+    }
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
     export let data;
 </script>
 
+<form method="POST">
+    <input name="search" type="text" placeholder="Search here" />
+</form>
+
 {#each data.hits as post}
     <h1>{post.title}</h1>
     <div>{@html post.body}</div>
