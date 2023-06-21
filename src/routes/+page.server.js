22836639317fcc7f6f8cc4d38d19f58fa46b19af
Stéphane Klein <contact@stephane-klein.info>
2023-06-21 16:32:25 +0200

    Add server side data page

---
+++ src/routes/+page.server.js
@@ -0,0 +1,14 @@
+export function load() {
+    return {
+        posts: [
+            {
+                'title': 'aaa',
+                'body': 'foobar'
+            },
+            {
+                'title': 'bbb',
+                'body': 'foobar2'
+            }
+        ]
+    };
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    export let data;
+</script>
+
+{#each data.posts as post}
+    <h1>{post.title}</h1>
+    <div>{@html post.body}</div>
+{/each}
