174a484904c9453f6af6a6f7ff6976ee52874a09
Stéphane Klein <contact@stephane-klein.info>
2023-08-06 22:34:28 +0200

    Don't display post body content in list pages

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -5,26 +5,25 @@
 </script>
 
 {#each data.hits as post}
-    <article class="mt-8 mb-16">
+    <article class="my-6">
         <h1 class="text-2xl font-bold underline">
             <a
                 href={`/posts/${post.slug}/`}
             >{@html post?._formatted?.title ?? post?.title}</a>
         </h1>
 
-        <div class="my-2">Publié le : {post?.date}</div>
+        <div class="inline-flex items-center gap-x-1.5">
+            <div class="my-2">Publié le : {post?.date}</div>
 
-        <ul class="flex flex-row gap-2 my-2">
-            {#each post?.tags as tag}
-                <li
-                    class="inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-gray-900 ring-1 ring-inset ring-gray-200"
-                >
-                    <a href="?q=%23{tag}">#{tag}</a>
-                </li>
-            {/each}
-        </ul>
-        <div class="prose lg:prose-lg max-w-none">
-            {@html markdownIt.render(post?._formatted?.body ?? post?.body)}
+            <ul class="flex flex-row gap-2 my-2">
+                {#each post?.tags as tag}
+                    <li
+                        class="inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-gray-900 ring-1 ring-inset ring-gray-200"
+                    >
+                        <a href="?q=%23{tag}">#{tag}</a>
+                    </li>
+                {/each}
+            </ul>
         </div>
     </article>
 {/each}
